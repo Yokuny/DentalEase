1184f62326799dcd208655185645687f2e2983a4
Felipe Rangel Ribeiro <felipe.vni@hotmail.com>
2024-01-25 23:20:12 -0300

    build: changing project to a new front-end library
    passing to https://ui.shadcn.com

--- app/app/layout.tsx
+++
@@ -1,7 +0,0 @@
-import type { ChildrenProps } from "@/types";
-
-const Layout = ({ children }: ChildrenProps) => {
-  return <div className="py-6">{children}</div>;
-};
-
-export default Layout;
--- app/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default async function Home() {
-  return (
-    <section className="flex flex-col items-center gap-3 pb-6 md:py-6 py-2">
-      <h1>AGENDA</h1>
-    </section>
-  );
-}
